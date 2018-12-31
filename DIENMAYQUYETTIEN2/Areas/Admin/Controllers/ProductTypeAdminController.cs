b9097df324530665acc0818481209b5557e2939f
Tuan Heo <tuanheotk@gmail.com>
2018-12-31 17:12:55 +0700

    Them chuc nang Product Type  trang index

---
+++ DIENMAYQUYETTIEN2/Areas/Admin/Controllers/ProductTypeAdminController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DIENMAYQUYETTIEN2.Models;
+using System.Web.Security;
+
+namespace DIENMAYQUYETTIEN2.Areas.Admin.Controllers
+{
+    public class ProductTypeAdminController : Controller
+    {
+        DmQT10Entities db = new DmQT10Entities();
+        // GET: Admin/ProductTypeAdmin
+        public ActionResult Index()
+        {
+            if (Session["UserName"] != null)
+            {
+                return View(db.ProductTypes.ToList());
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
+        //Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(ACCOUNT acc)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DmQT10Entities db = new DmQT10Entities())
+                {
+                    var obj = db.ACCOUNTs.Where(a => a.USERNAME.Equals(acc.USERNAME) && a.PASSWORD.Equals(acc.PASSWORD)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["Username"] = obj.USERNAME.ToString();
+                        Session["FullName"] = obj.FULLNAME.ToString();
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(acc);
+        }
+        //Logout
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon(); // it will clear the session at the end of request
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
